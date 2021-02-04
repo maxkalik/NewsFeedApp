10e2dc428d5edc460a58019d5769538246a39b29
Maksim Kalik <maxkalik@gmail.com>
2021-02-04 12:09:08 +0200

    Initial Commit

---
+++ NewsFeedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsFeedApp
+//
+//  Created by Maksim Kalik on 2/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
